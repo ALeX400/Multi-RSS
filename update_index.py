96e37c0f8f45c3dd73c8606700b30c3920cecdec
@LeX <ghebaura34@gmail.com>
2024-05-26 02:57:34 +0300

    Update update_index.py

--- update_index.py
+++ update_index.py
@@ -1,5 +1,5 @@
 import os
-
+from datetime import datetime
 
 def update_index():
     rss_dir = 'rss'
@@ -7,10 +7,18 @@ def update_index():
     index_path = os.path.join(rss_dir, 'index.md')
 
     with open(index_path, 'w') as index_file:
-        index_file.write('# List of XML files in this directory\n\n')
+        # Write the header and description
+        index_file.write('# XML Files Index\n')
+        index_file.write('This index was automatically generated to list all XML files in the `rss` directory.\n\n')
+        index_file.write(f'_Last updated: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}_\n\n')
+        
+        # Write the table header
+        index_file.write('| File Name | Link |\n')
+        index_file.write('|-----------|------|\n')
+        
+        # Write each file as a row in the table
         for file in files:
-            index_file.write(f'- [Link to {file}](./{file})\n')
-
+            index_file.write(f'| {file} | [Link to {file}](./{file}) |\n')
 
 if __name__ == "__main__":
     update_index()
