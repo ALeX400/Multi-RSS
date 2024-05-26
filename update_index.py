bd4205bc4bc1f4cfd7c7a338b7eccc56df13a67b
@LeX <ghebaura34@gmail.com>
2024-05-26 03:16:21 +0300

    Update update_index.py

--- update_index.py
+++ update_index.py
@@ -7,18 +7,45 @@ def update_index():
     index_path = os.path.join(rss_dir, 'index.md')
 
     with open(index_path, 'w') as index_file:
-        # Write the header and description
-        index_file.write('# XML Files Index\n')
-        index_file.write('This index was automatically generated to list all XML files in the `rss` directory.\n\n')
-        index_file.write(f'_Last updated: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}_\n\n')
-        
-        # Write the table header
-        index_file.write('| File Name | Link |\n')
-        index_file.write('|-----------|------|\n')
-        
+        # Write the custom header with SVG
+        index_file.write('''
+<svg width="800" height="200">
+  <rect width="800" height="200" fill="#f3f4f6"/>
+  <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-family="Arial" font-size="24" fill="#333">XML Files Index</text>
+</svg>
+
+This index was automatically generated to list all XML files in the `rss` directory.
+
+_Last updated: {datetime_now}_
+
+'''.format(datetime_now=datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+
+        # Write the table header with SVG decorations
+        index_file.write('''
+<table>
+  <thead>
+    <tr>
+      <th>File Name</th>
+      <th>Link</th>
+    </tr>
+  </thead>
+  <tbody>
+''')
+
         # Write each file as a row in the table
         for file in files:
-            index_file.write(f'| {file} | [Link to {file}](./{file}) |\n')
+            index_file.write(f'''
+    <tr>
+      <td>{file}</td>
+      <td><a href="./{file}">Link to {file}</a></td>
+    </tr>
+''')
+
+        # Close the table
+        index_file.write('''
+  </tbody>
+</table>
+''')
 
 if __name__ == "__main__":
     update_index()
