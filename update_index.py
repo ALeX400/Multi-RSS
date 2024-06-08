3a96351dd721d014931db559e4da1281954b24be
@LeX <ghebaura34@gmail.com>
2024-06-08 17:56:57 +0300

    Update update_index.py

--- update_index.py
+++ update_index.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+import urllib.parse
 
 def update_index():
     rss_dir = 'rss'
@@ -16,12 +17,19 @@ def update_index():
         os.remove(index_path)
         print(f"Existing '{index_path}' deleted.")
 
-    files = [f for f in os.listdir(rss_dir) if f.endswith('.xml')]
+    # Traverse directory to find all XML files
+    files = []
+    for root, dirs, filenames in os.walk(rss_dir):
+        for filename in filenames:
+            if filename.endswith('.xml'):
+                # Get the relative path to the file from rss_dir
+                relative_path = os.path.relpath(os.path.join(root, filename), rss_dir)
+                files.append(relative_path)
 
     with open(index_path, 'w') as index_file:
         # Write the header and description
         index_file.write('# XML Files Index\n')
-        index_file.write('**This index was automatically generated to list all XML files in the `rss` directory.**\n\n')
+        index_file.write('**This index was automatically generated to list all XML files in the `rss` directory and its subdirectories.**\n\n')
         index_file.write(f'**Last updated: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}**\n\n')
         
         # Write the table header
@@ -30,7 +38,8 @@ def update_index():
         
         # Write each file as a row in the table
         for file in files:
-            index_file.write(f'| {file} | [Link to {file}](./{file}) |\n')
+            encoded_file = urllib.parse.quote(file)
+            index_file.write(f'| {file} | [Link to {file}](./{encoded_file}) |\n')
 
     print(f"Index updated with {len(files)} files.")
 
